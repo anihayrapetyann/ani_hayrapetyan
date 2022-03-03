9136eb1c2c1ec188bc8cc0e7a9967eb587464dca
Ani <ani_hayrapetyan00@mail.ru>
2022-03-03 22:15:54 +0400

    Added third exercise

---
+++ cpp/day1/3_leap.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int year=2022;
+        int i=4;
+	int n=0;
+	while(n<20){
+
+        if(year % i == 0){
+ 	cout << year <<endl;
+	n++;	
+	
+}
+	year++;
+
+
+}
+
+
+
+return 0;
+}
+
+
+
