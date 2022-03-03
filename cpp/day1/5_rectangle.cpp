8397a8b2befca27e3c4a22da7ff2e99623e68fda
Ani <ani_hayrapetyan00@mail.ru>
2022-03-03 22:19:10 +0400

    Added fifth exercise

---
+++ cpp/day1/5_rectangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int length;
+	int width;
+	int perimeter;
+	int area;
+	cout << "Input length of rectangle\n";
+	cin >> length;
+	cout << "Input the width of rectangle\n";
+	cin >> width;
+        perimeter=(length + width) * 2;
+	cout << "The perimeter of rectangle is " << perimeter <<endl;
+	area=length * width;
+	cout << "the area of rectangle is " << area <<endl;
+	
+
+
+return 0;
+}
+
+
