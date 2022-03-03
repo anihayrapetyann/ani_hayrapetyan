18bbe345f3b5486e4fee3afe4c36fde6772a18b8
Ani <ani_hayrapetyan00@mail.ru>
2022-03-03 22:17:39 +0400

    Added fourth exercise

---
+++ cpp/day1/4_fermer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int chicken;
+        int cow;
+        int pig;
+        int sum;
+        cout << "chicken - ";
+        cin >> chicken;
+        cout << "cow - ";
+        cin >> cow;
+        cout << "pig - ";
+        cin >> pig;
+        sum = (chicken * 2) + (cow * 4) + (pig * 4);
+        cout << sum <<endl;
+
+	return 0;
+}
+
