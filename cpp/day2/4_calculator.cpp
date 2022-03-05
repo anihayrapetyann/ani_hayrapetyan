c58918819b78ed34ce4bdf3a8744da3362ad9c72
Ani <ani_hayrapetyan00@mail.ru>
2022-03-05 21:06:26 +0400

    Added 4th file

---
+++ cpp/day2/4_calculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+        double firstnumber;
+	double secondnumber;
+	char oper;
+        cout << "Enter the  first number \n";
+	cin >> firstnumber;
+	cout << "Enter the operator (add, subtract, multiply and divide)\n";
+	cin >> oper;
+	cout << "Enter the second number\n";
+        cin >> secondnumber;
+        switch (oper) {
+                case '+':
+                        cout << firstnumber << '+' << secondnumber << '=' <<firstnumber + secondnumber << endl;
+			break;
+		case '-':
+                        cout << firstnumber << '-' << secondnumber << '=' <<firstnumber - secondnumber << endl;
+
+                        break;
+		case '*':
+                        cout << firstnumber << '*' << secondnumber << '=' <<firstnumber * secondnumber << endl;
+
+                        break;
+		case '/':
+                        cout << firstnumber << '/' << secondnumber << '=' <<firstnumber / secondnumber << endl;
+
+                        break;
+                
+                default:
+                        cout << "Please, try again";
+                        break;
+        }
+        return 0;
+}
+
