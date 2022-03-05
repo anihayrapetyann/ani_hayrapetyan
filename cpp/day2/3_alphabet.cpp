1406fb886b32788d1d82fdd5f1a1e2d87870a1ff
Ani <ani_hayrapetyan00@mail.ru>
2022-03-05 21:05:20 +0400

    Added 3rd file

---
+++ cpp/day2/3_alphabet.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+        char letter;
+        cout << "Input the letter \n";
+        cin >> letter;
+        switch (letter) {
+                case 'a':
+                        cout << "The letter is vowel \n";
+                        break;
+                case 'e':
+                        cout << "The letter is vowel \n";
+                        break;
+                case 'i':
+                        cout << "The letter is vowel \n";
+                        break;
+                case 'u':
+                        cout << "The letter is vowel \n";
+                        break;
+                case 'o':
+                        cout << "The letter is vowel \n";
+                        break;
+                case 'A':
+                        cout << "The letter is vowel \n";
+                        break;
+                case 'E':
+                        cout << "The letter is vowel \n";
+                        break;
+		case 'I':
+                        cout << "The letter is vowel \n";
+                        break;
+		case 'U':
+                        cout << "The letter is vowel \n";
+                        break;
+		case 'O':
+                        cout << "The letter is vowel \n";
+                        break;
+
+                default:
+                        cout << "The letter is consonant\n";
+                        break;
+        }
+        return 0;
+}
+
