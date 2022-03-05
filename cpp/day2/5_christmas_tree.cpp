b6c61d2eebd4b7026a1a932b836669abf4292f51
Ani <ani_hayrapetyan00@mail.ru>
2022-03-05 21:08:36 +0400

    Added 5th file

---
+++ cpp/day2/5_christmas_tree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main () {	        
+       	int n;
+	cout << "Enter the height\n";
+	cin >> n;
+	const int height = n;
+
+	cout << "This is my first triangle\n";
+	int i = height;
+	while (i > 0){
+		int j = 1;
+		while (j <= i){
+			cout << "*";
+			j++;
+		}		     
+		cout << endl;
+		i--;
+	}
+
+	cout << endl << "this is my second triangle\n";
+	i = 1;
+	while (i <= height){
+               int j = 1;
+               while (j <= i){
+                        cout << "*";
+                        j++;
+                }
+        	cout << endl;
+       		i++;
+        }
+
+	cout << endl << "this is my third triangle\n";
+        i = 1;
+        while (i <= height){
+                int j = height;
+                while (j >= 0){
+                        if(j < i){
+                                cout << "*";
+                        }
+                        else{
+                                cout << " ";
+                        }
+                        j--;
+                }
+                cout << endl;
+                i++;
+        }
+ 
+	cout << endl << "this is my fourth triangle\n";
+	i = 1;
+	while (i <= height) {
+		int j = 1;
+		while(j <= height - i){
+			cout <<"  ";
+			j++;
+        	}
+		j = 0;
+        	while(j != 2 * i - 1){
+           		 cout << "* ";
+           		 ++j;
+		}
+		cout << endl;
+		i++;
+		j = 0;
+	}
+
+	return 0;
+}
+
