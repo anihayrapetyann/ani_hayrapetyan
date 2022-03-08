fe7d7172605145a062cb0f9c2b8bc0fb3b0c3962
Ani <ani_hayrapetyan00@mail.ru>
2022-03-08 11:29:57 +0400

    corrections of mistakes

--- cpp/day2/2_month.cpp
+++ cpp/day2/2_month.cpp
@@ -7,42 +7,23 @@ int main () {
         cin >> monthnumber;
         switch (monthnumber) {
                 case 1:
-                        cout << "Total number of days = 31 \n";
-                        break;
-                case 2:
-                        cout << "Total number of days = 28 \n(for leap years total number of days = 29) \n ";
-                        break;
                 case 3:
-                        cout << "Total number of days = 31 \n";
-                        break;
-                case 4:
-                        cout << "Total number of days = 30 \n";
-                        break;
                 case 5:
-                        cout << "Total number of days = 31 \n";
-                        break;
-                case 6:
-                        cout << "Total number of days = 30 \n";
-                        break;
                 case 7:
-                        cout << "Total number of days = 31 \n";
-                        break;
 		case 8:
-                        cout << "Total number of days = 31 \n";
-                        break;
-		case 9:
-                        cout << "Total number of days = 30 \n";
-                        break;
 		case 10:
-                        cout << "Total number of days = 31 \n";
-                        break;
-		case 11:
-                        cout << "Total number of days = 30 \n";
-                        break;
 		case 12:
                         cout << "Total number of days = 31 \n";
                         break;
-
+		case 2:
+                        cout << "Total number of days = 28 \n(for leap years total number of days = 29) \n ";
+                        break;
+		case 4:
+		case 6:
+		case 9:	
+		case 11:
+			cout << "Total number of days = 30 \n";
+                        break;
                 default:
                         cout << "The year has 12 months \n";
                         break;
