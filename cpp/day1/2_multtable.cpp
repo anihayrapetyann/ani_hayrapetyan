30d4b29591912a29d9bddfc71297063e003949b4
Ani <ani_hayrapetyan00@mail.ru>
2022-04-04 01:54:37 +0400

    minor fix

--- cpp/day1/2_multtable.cpp
+++ cpp/day1/2_multtable.cpp
@@ -11,7 +11,7 @@ int main() {
         while (i <= 10) {
 		multiple = number * i;
         	cout << number << " * " << i << " = " << multiple <<endl;
-       	i++;  
+		i++;  
 	}
 
         return 0;
