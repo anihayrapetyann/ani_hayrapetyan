90db5dfff451d4610e2b9830d07ebff05f5d55bc
Arman <shahramanyan1995@mail.ru>
2022-04-27 11:33:18 +0400

    corrected coding style

--- data_structures/day4/Sorting/sort.cpp
+++ data_structures/day4/Sorting/sort.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <time.h>
 #include <random>
+
 using namespace std;
+
 mt19937 gen (time(0)); 
 
 int MyGenerator() {
@@ -9,9 +11,7 @@ int MyGenerator() {
     return rand;
 }
 
-int main()
-{   
-    
+int main() {       
     const int size = 20000;
     int arr1[size];
     int arr2[size];
@@ -23,10 +23,10 @@ int main()
    
     clock_t start1 = clock();
 
-    for(int  i = 0; i < size; i++){
+    for (int  i = 0; i < size; i++){
         bool checkSwap = false;
-        for(int j = 0; j < size - i - 1; j++){
-            if(arr1[j] > arr1[j + 1]){
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr1[j] > arr1[j + 1]) {
                 int temp = arr1[j];
                 arr1[j] = arr1[j + 1];
                 arr1[j + 1] = temp;
@@ -40,6 +40,7 @@ int main()
     
     clock_t end1 = clock();
     double elapsed1 = double(end1- start1)/CLOCKS_PER_SEC;
+
     cout << "Bubble Sorting" << "\n" << "Size = " << size << "\n";
     cout <<  "Run Time = " << elapsed1 * 1000;
     cout << endl << endl;
@@ -51,12 +52,11 @@ int main()
     cout << endl;
     */
 
-   clock_t start2 = clock();
+    clock_t start2 = clock();
    
-    
-    for(int i = 1; i < size; i++ ){
-        for(int j = i; j  > 0; j--){
-            if(arr2[j] < arr2[j - 1]){
+    for (int i = 1; i < size; i++ ) {
+        for (int j = i; j  > 0; j--) {
+            if (arr2[j] < arr2[j - 1]) {
                 int temp = arr2[j];
                 arr2[j] = arr2[j - 1];
                 arr2[j - 1] = temp;
@@ -66,18 +66,16 @@ int main()
             }
         }
     }
+
     clock_t end2 = clock();
     double elapsed2 = double(end2- start2)/CLOCKS_PER_SEC;
     cout << "Insertion Sorting" << "\n" << "Size = " << size << "\n";
     cout <<  "Run Time = " << elapsed2 * 1000;
     cout << endl;
     
-    
-    for(int  i = 0; i < size; i++) {
-        
+    for (int  i = 0; i < size; i++) {
         cout << arr2[i] << " ";
     }
-    
     
     return 0;
 }
