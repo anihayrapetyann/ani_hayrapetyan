10ce0cb37928a56a6af90a43419a066921bb289b
Ani <ani_hayrapetyan00@mail.ru>
2022-05-09 21:42:56 +0400

    correction of mistakes

--- data_structures/day8/graph.hpp
+++ data_structures/day8/graph.hpp
@@ -9,14 +9,16 @@ using namespace std;
 class Graph {
 public:
     int _size;
-    int matrix[INT8_MAX][INT8_MAX];
+    bool** matrix;
 
 public:
     Graph(int size);
+    ~Graph();
     void addEdge(int a, int b);
     void removeEdge(int a, int b);
     void addNode();
-    void removeNode(int nodenumber);
+    void removeNode();
+    bool isNodeExists(int node);
     bool isEdgeExists(int node1, int node2);
     void nodeNeighbours(int node); 
     void print();
@@ -24,69 +26,82 @@ public:
 
 Graph::Graph(int size) {
     _size = size;
+    matrix = new bool*[_size];
     for (int i = 0; i < _size; i++) {
+        matrix[i] = new bool[size];
         for (int j = 0; j < _size; j++) {
-                matrix[i][j] = 0;
+                matrix[i][j] = false;
             }
         }
     }
 
-void Graph::addEdge(int a, int b) {
-    if (a > _size - 1 || b > _size - 1 || a < 0 || b < 0) {
-        cout << "The node is not exists !" << endl;
-    }
-    else if (matrix[a][b] == 1 && matrix[b][a] == 1 ) {
-        cout << "The edge is already exists " << endl;
-    }
-    else {
-        for (int i = 0; i < _size; i++) {
-            for (int j = 0; j < _size; j++) {
-                matrix[a][b] = 1;
-                matrix[b][a] = 1;
-            }
+Graph::~Graph(){
+        for (int i =0; i < _size; i++) {
+            delete[] matrix[i];
         }
+        delete[] matrix;
     }
+
+
+void Graph::addEdge(int a, int b) {
+    if (isNodeExists(a) && isNodeExists(b) && !isEdgeExists(a, b)) {
+                matrix[a][b] = true;
+                matrix[b][a] = true; 
+    }
+    else  
+        cout << "Action can not be perfomed" << endl;
+    
 }
 
 void Graph::removeEdge(int a, int b) {
-    if (a > _size - 1 || b > _size - 1 || a < 0 || b < 0) {
-        cout << "The node is not exists !" << endl;
-    }
-    else if (matrix[a][b] == 0) {
-        cout << "there is no edge  " << endl;
-    }
-    else {
-        for (int i = 0; i < _size; i++) {
-            for (int j = 0; j < _size; j++) {
+    if (isNodeExists(a) && isNodeExists(b) && isEdgeExists(a, b)) {
+      
                 matrix[a][b] = 0;
                 matrix[b][a] = 0;
-            }
-        }
     }
+    else  
+        cout << "Action can not be perfomed" << endl;
+       
 }
 
 void Graph::addNode() {
-    _size++;
+    ++_size;
+    bool** newmatrix = new bool*[_size];
     for (int i = 0; i < _size; i++) {
+            newmatrix[i] = new bool[_size]; 
         for (int j = 0; j < _size; j++) {
-            matrix[i][_size - 1] = 0;
-            matrix[_size - 1][j] = 0;
+            if (i != _size - 1 && j != _size - 1)
+            newmatrix[i][j] = matrix[i][j];
+            if(i != _size - 1 && j != _size - 1)
+             matrix[i][j] = false;
         }
     }
+    delete matrix;
+    matrix = newmatrix;
 }
 
-void Graph::removeNode(int nodenumber) {
+void Graph::removeNode() {
+    --_size;
+    bool** newmatrix = new bool*[_size];
     for (int i = 0; i < _size; i++) {
+        newmatrix[i] = new bool[_size]; 
         for (int j = 0; j < _size; j++) {
-            matrix[i][nodenumber] = matrix[i][nodenumber + 1];
-            matrix[nodenumber][j] = matrix[nodenumber + 1][j];
+            newmatrix[i][j] = matrix[i][j];
         }
     }
-    _size--;
+    delete matrix;
+    matrix = newmatrix;
+}
+
+bool Graph::isNodeExists(int node){ 
+    if (node > _size - 1 || node < 0 ) {
+        return false;
+    }
+    return true;
 }
 
 bool Graph::isEdgeExists(int node1, int node2) {
-    if (matrix[node1][node2] == 1 && matrix[node2][node1] == 1) {
+    if (matrix[node1][node2]) {
         return true;
     }
     return false;
@@ -99,7 +114,6 @@ void Graph::nodeNeighbours(int node) {
         }
     }
 }
-
 void Graph::print() {
     for (int i = 0; i < _size; i++) {
         for (int j = 0; j < _size; j++) {
--- data_structures/day8/main.cpp
+++ data_structures/day8/main.cpp
@@ -3,18 +3,15 @@
 using namespace std;
 
 int main() {
-
    Graph graph(4);
    graph.print();
-   graph.addEdge(1, 2);
-   graph.addEdge(0, 1);
-   graph.addEdge(3, 1);
-   graph.addEdge(2, 3);
-   graph.removeEdge(0, 1);
+   graph.addEdge(0,0);
+   graph.addEdge(1, 3);
    graph.addNode();
-   graph.removeNode(3);
    graph.print();
-   cout << graph.isEdgeExists(1, 2) << endl;
+   graph.removeNode();
+   graph.print();
+   cout << graph.isEdgeExists(1, 3) << endl;
    graph.nodeNeighbours(1);
 
     return 0;
