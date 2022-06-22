9ee21355fe2236d6dd334aa38040e5b1c51b709d
Ani <ani_hayrapetyan00@mail.ru>
2022-06-22 19:18:55 +0400

    Added files for 5th task

---
+++ web_development/js/day4/calculator/script.js
@@ -0,0 +1,52 @@
+function addFunction() {
+    let number1 =Number( document.getElementById("element1").value);
+    let number2 =Number( document.getElementById("element2").value);
+    const result = number1 + number2;
+    const container = document.getElementById('section1');
+    document.getElementById("result").innerHTML=String(result);
+    document.getElementById('operation').innerHTML = "+";
+    return (number1 + number2);
+}
+
+function subtractFunction() {
+    let number1 =Number( document.getElementById("element1").value);
+    let number2 =Number( document.getElementById("element2").value);
+    const result = number1 - number2;
+    const container = document.getElementById('section1');
+    document.getElementById("result").innerHTML=String(result);
+    document.getElementById('operation').innerHTML = "-";
+    return (number1 - number2);
+}
+
+function multiplyFunction() {
+    let number1 =Number( document.getElementById("element1").value);
+    let number2 =Number( document.getElementById("element2").value);
+    const result = number1 * number2;
+    const container = document.getElementById('section1');
+    document.getElementById("result").innerHTML=String(result);
+    document.getElementById('operation').innerHTML = "*";
+    return (number1 * number2);
+}
+
+function devideFunction() {
+    document.getElementById('operation').innerHTML = "/";
+    let number1 =Number( document.getElementById("element1").value);
+    let number2 =Number( document.getElementById("element2").value);
+    const result = number1 / number2;
+    const container = document.getElementById('section1');
+    document.getElementById("result").innerHTML=String(result);
+ 
+    return (number1 / number2);
+}
+
+function clearFunction() {
+    document.getElementById("element1").value = "";
+    document.getElementById("element2").value = "";
+    document.getElementById("result").innerHTML = "";
+}
+
+document.addEventListener('keydown', function(event){
+    if(event.key === "Escape") {
+        clearFunction();
+    }
+});
